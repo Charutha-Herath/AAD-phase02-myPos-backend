4f2d7b26b5771a0cec69c10d77d1e25162096f8d
Charutha-Herath <charuh2023@gmail.com>
2024-10-13 23:15:33 +0530

    add AppInit

--- src/main/java/lk/ijse/phase02/AppInit.java
+++ src/main/java/lk/ijse/phase02/AppInit.java
@@ -1,4 +1,30 @@
 package lk.ijse.phase02;
 
-public class AppInit {
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.phase02.config.WebAppConfig;
+import lk.ijse.phase02.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        String tempDir = System.getProperty("java.io.tmpDir");
+        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
+    }
 }
