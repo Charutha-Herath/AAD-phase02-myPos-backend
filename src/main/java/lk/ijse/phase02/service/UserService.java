354674eaffbe2b4d5f0ae20edde0ef55611b1704
Charutha-Herath <charuh2023@gmail.com>
2024-10-14 10:51:03 +0530

    add saveUser method

---
+++ src/main/java/lk/ijse/phase02/service/UserService.java
@@ -0,0 +1,8 @@
+package lk.ijse.phase02.service;
+
+import lk.ijse.phase02.dto.impl.UserDTO;
+
+public interface UserService {
+
+    void saveUser(UserDTO userDTO);
+}
---
+++ src/main/java/lk/ijse/phase02/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package lk.ijse.phase02.service.impl;
+
+import lk.ijse.phase02.dto.impl.UserDTO;
+import lk.ijse.phase02.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void saveUser(UserDTO userDTO) {
+
+    }
+}
