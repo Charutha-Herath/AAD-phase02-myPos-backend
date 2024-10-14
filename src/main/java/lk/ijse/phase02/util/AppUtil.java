daf3441459fa13b27a3de62a9801a373b2151da0
Charutha-Herath <charuh2023@gmail.com>
2024-10-14 10:07:06 +0530

    add AppUtil

---
+++ src/main/java/lk/ijse/phase02/util/AppUtil.java
@@ -0,0 +1,10 @@
+package lk.ijse.phase02.util;
+
+import java.util.UUID;
+
+public class AppUtil {
+
+    public static String generateUserId(){
+        return "USER-"+ UUID.randomUUID();
+    }
+}
