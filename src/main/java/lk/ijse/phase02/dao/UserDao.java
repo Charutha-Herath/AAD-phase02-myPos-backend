c9b3e25d6cc7e52eed4c9673565720fffd8968b7
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 14:56:58 +0530

    done UserDao

---
+++ src/main/java/lk/ijse/phase02/dao/UserDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.phase02.dao;
+
+import lk.ijse.phase02.entity.impl.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity,String> {
+}
