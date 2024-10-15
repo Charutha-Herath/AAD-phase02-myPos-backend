b0fec154fe1894f91bfd03c4814898ef6f34a666
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 17:34:47 +0530

    add error status class

---
+++ src/main/java/lk/ijse/phase02/customStatusCodes/SelectedUserErrorStatus.java
@@ -0,0 +1,15 @@
+package lk.ijse.phase02.customStatusCodes;
+
+
+import lk.ijse.phase02.dto.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SelectedUserErrorStatus implements UserStatus {
+    private int statusCode;
+    private String statusMessage;
+}
