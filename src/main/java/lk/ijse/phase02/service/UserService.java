9e555879b3d7004d67406e693d118dfb35976939
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 17:35:30 +0530

    done getUserByEmail

--- src/main/java/lk/ijse/phase02/service/UserService.java
+++ src/main/java/lk/ijse/phase02/service/UserService.java
@@ -1,8 +1,11 @@
 package lk.ijse.phase02.service;
 
+import lk.ijse.phase02.dto.UserStatus;
 import lk.ijse.phase02.dto.impl.UserDTO;
 
 public interface UserService {
 
     void saveUser(UserDTO userDTO);
+
+    UserStatus getUserByEmail(String email);
 }
