b9e31f358d7c842e05d0bf18104b2b8e2d09ff35
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 17:39:39 +0530

    implemented UserDTO in UserStatus

--- src/main/java/lk/ijse/phase02/dto/impl/UserDTO.java
+++ src/main/java/lk/ijse/phase02/dto/impl/UserDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.phase02.dto.impl;
 
+import lk.ijse.phase02.dto.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class UserDTO {
+public class UserDTO implements UserStatus {
     private String userId;
     private String name;
     private String email;
