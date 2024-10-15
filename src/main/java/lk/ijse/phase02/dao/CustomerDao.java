5a6a06a39e8dc107c7a32e777aa5a5a2070fafce
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 22:55:08 +0530

    add CustomerDao

---
+++ src/main/java/lk/ijse/phase02/dao/CustomerDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.phase02.dao;
+
+import lk.ijse.phase02.entity.impl.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends JpaRepository<CustomerEntity,String> {
+}
