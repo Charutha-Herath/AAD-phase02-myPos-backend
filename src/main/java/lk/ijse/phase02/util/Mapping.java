0bfaba8a5ad294660957b3f0c4554ece9d20b2c0
Charutha-Herath <charuh2023@gmail.com>
2024-10-15 14:58:04 +0530

    done toUserEntity

--- src/main/java/lk/ijse/phase02/util/Mapping.java
+++ src/main/java/lk/ijse/phase02/util/Mapping.java
@@ -1,6 +1,8 @@
 package lk.ijse.phase02.util;
 
 
+import lk.ijse.phase02.dto.impl.UserDTO;
+import lk.ijse.phase02.entity.impl.UserEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,9 @@ public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
 
-
+    public UserEntity toUserEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
 
 
 
